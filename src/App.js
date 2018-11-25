5bf5088118a9dc801570094c2ba18d38e79d5a8d
unknown <you@example.com>
2018-11-25 23:12:30 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+import { Header } from './components/common';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import LibraryList from './components/LibraryList';
+
+const App = () => {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={{flex: 1}}>
+          <Header headerText="Tech Stack" />
+          <LibraryList />
+        </View>
+      </Provider>
+    );
+};
+export default App;
